6a3401b3cea88c04124b45dab0861aca272c968f
Cory Rabiea <m00tzmanAlmighty@gmail.com>
2017-09-19 11:53:36 -0400

    initial show trigger function created

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+    var target = $(".trig").offset().top;
+    var interval = setInterval(function () {
+        if ($(window).scrollTop() >= target) {
+            console.log("it works!");
+            $("#skills-list li").each(function () {
+                $(this).next().show(1000);
+            })
+            clearInterval(interval);
+        }
+    }, 250);
+}
